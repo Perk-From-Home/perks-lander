24765f30034e38f351cb5f9885f12ee5811bc991
Archy de Berker <archy.deberker@gmail.com>
2020-07-14 15:57:12 -0400

    Fiddle with flex and footer text

--- src/pages/index.js
+++ src/pages/index.js
@@ -128,7 +128,7 @@ export default () => (
         <LabelText className="mb-8 text-gray-600 text-center">What customers are saying</LabelText>
         <div className="flex flex-col md:flex-row md:-mx-3">
           {customerData.map(customer => (
-            <div key={customer.customerName} className="flex-1 px-3">
+            <div key={customer.customerName} className="flex-1 px-3 flex">
               <CustomerCard customer={customer} />
             </div>
           ))}
@@ -136,10 +136,8 @@ export default () => (
       </div>
     </section>
     <section className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
-      <h3 className="text-5xl font-semibold">Ready to grow your business?</h3>
-      <p className="mt-8 text-xl font-light">
-        Quis lectus nulla at volutpat diam ut. Enim lobortis scelerisque fermentum dui faucibus in.
-      </p>
+      <h3 className="text-5xl font-semibold">Ready to delight your employees?</h3>
+      <p className="mt-8 text-xl font-light">Get in touch about our early access program</p>
       <p className="mt-8">
         <Button size="xl">Get Started Now</Button>
       </p>
