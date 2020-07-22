eeaeb1aab50bcdfa325b80f185ff1c8da84573af
Archy de Berker <archy.deberker@gmail.com>
2020-07-22 15:29:00 -0400

    Don't show menu on mobile, reduce size of header image on mobile

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,30 +7,30 @@ import Button from '../Button';
 import { CUSTOMER_GFORM_URL } from '../../data/external-links';
 
 const Header = () => (
-  <header className="sticky top-0 bg-white shadow">
+  <header className="sticky top-0 bg-white shadow bg-opacity-75">
     <div style={{ margin: '0 auto' }}></div>
     <div className="container flex flex-col lg:flex-row justify-between items-center mx-auto py-4 px-8">
-      <div className="text-2xl lg:pl-0 text-center">
+      <div className="text-2xl text-center">
         <Logo />
       </div>
 
-      <div className="flex mt-4 :mt-0 md:visible pb-4 ">
-        <AnchorLink className="px-4 text-black" href="#features">
+      <div className="flex md:mt-4 hidden md:inline md:pb-4 ">
+        <AnchorLink className="px-4 text-black text-center p-1" href="#features">
           How It Works
         </AnchorLink>
-        <AnchorLink className="px-4 text-black" href="#services">
+        <AnchorLink className="px-4 text-black text-center p-1" href="#services">
           Benefits
         </AnchorLink>
-        <AnchorLink className="px-4 text-black" href="#stats">
+        <AnchorLink className="px-4 text-black text-center p-1" href="#stats">
           Our Partners
         </AnchorLink>
-        <AnchorLink className="px-4 text-black" href="#testimonials">
+        <AnchorLink className="px-4 text-black text-center p-1" href="#testimonials">
           Testimonials
         </AnchorLink>
       </div>
       <div className="hidden lg:block">
         <Button className="text-sm" href={CUSTOMER_GFORM_URL}>
-          Start Free Trial
+          Get Early Access
         </Button>
       </div>
     </div>
--- src/png/icons.js
+++ src/png/icons.js
@@ -5,7 +5,9 @@ import profileIcons from './profile.png';
 import logo from './PFH.png';
 
 const Icon = img => <img src={img} className="w-24 mx-auto mb-2" />;
-const PFHLogo = img => <img src={img} style={{ height: 100, margin: '0 auto' }} />;
+const PFHLogo = img => (
+  <img src={img} className="object-contain h-20 md:h-32 mb-2" style={{ margin: '0 auto' }} />
+);
 
 const Truck = () => Icon(truckIcon);
 const Settings = () => Icon(settingsIcon);
