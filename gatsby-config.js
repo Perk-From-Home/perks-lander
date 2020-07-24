63e786e838cb381e59013357b4b874fdbe5a5ed5
Archy de Berker <archy.deberker@gmail.com>
2020-07-24 09:38:57 -0400

    Add component and install for SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,16 @@
 module.exports = {
+  siteMetadata: {
+    title: `Perk from Home`,
+    description: `Linking WFH employees with great subscriptions`,
+    author: `Archy de Berker, Patrick Steeves, Claudel Rheault`
+  },
   plugins: [
     {
       resolve: `gatsby-theme-codebushi`,
       options: {
         tailwindConfig: `tailwind.config.js`
       }
-    }
+    },
+    `gatsby-plugin-react-helmet`
   ]
 };
